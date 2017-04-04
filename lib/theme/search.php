6809cf7a4be3f28e1e2d2dcd3826b71439c98728
Renato Teixeira Bueno <renatotbueno@gmail.com>
2017-04-04 16:10:31 -0300

    PROD-2980 add global variables because they are used on calendar themes

--- lib/theme/search.php
+++ lib/theme/search.php
@@ -100,6 +100,7 @@ class Ai1ec_Theme_Search extends Ai1ec_Base {
 	 */
 	protected function _replace_search_globals( array $variables_map ) {
         foreach ( $variables_map as $key => $current_value ) {
+            global $$key;
             $variables_map[$key] = $$key;
             $$key                = $current_value;
         }
