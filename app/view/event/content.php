a14a9eb9a9dfa09a6865a225a462d1ef304e9087
Calvin Yeh <calvin-yeh@users.noreply.github.com>
2017-04-27 16:21:41 -0300

    Update content.php

--- app/view/event/content.php
+++ app/view/event/content.php
@@ -153,15 +153,15 @@ HTML;
 			$matches
 		);
 		// Check if we have a result, otherwise try to find a featured image
-        if ( empty( $matches ) ) {
+		if ( empty( $matches ) ) {
+			$url_featured_image = ( ! empty ( wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) ) ) ? wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) ) : null );
 
-            $url_featured_image = ( ! empty ( wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) ) ) ? wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) ) : null );
-            if ( ! empty( $url_featured_image ) ) {
-                return $url_featured_image;
-            }
+			if ( ! empty( $url_featured_image ) ) {
+				return $url_featured_image;
+			}
 
-            return null;
-        }
+			return null;
+		}
 
 		// Mark found image.
 		$event->get( 'post' )->post_content = str_replace(
